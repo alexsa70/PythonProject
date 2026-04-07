af77585a8514f6ecf8845652ca6c06d1983fc98a
AlexL <alexsa70@gmail.com>
2026-04-07 08:12:11 +0300

    Test cases modified

--- tests/user_service/test_get_user.py
+++ tests/user_service/test_get_user.py
@@ -5,6 +5,10 @@ from src.clients.user_client import UserClient
 from src.schemas.user_schema import UserResponseSchema, RolesResponseSchema
 from src.utils.assertions import assert_status_code
 
+@pytest.fixture
+def users_client(api_client):
+    return UserClient(api_client)
+
 
 @pytest.mark.asyncio
 @pytest.mark.user_service
@@ -12,29 +16,28 @@ from src.utils.assertions import assert_status_code
 @allure.story("Get user")
 class TestUserService:
     @allure.title("Users: retrieve user by name")
-    async def test_get_user_by_name(self, api_client,admin_headers, admin_auth_context):
-        users_client = UserClient(api_client)
-
-        response = await users_client.get_user(
-            headers=admin_headers,
-            user_name=admin_auth_context.user.user_name,
-            )
-
-        assert_status_code(response, 200)
-
-        response_json = response.json()
-        parsed = UserResponseSchema.model_validate(response_json)
-        assert parsed.user_name == admin_auth_context.user.user_name
-
+    @allure.severity(allure.severity_level.CRITICAL)
+    async def test_get_user_by_name(self, api_client,admin_headers, admin_auth_context, users_client):
+        with allure.step("Send POST /user?name=... request"):
+            response = await users_client.get_user(
+                headers=admin_headers,
+                user_name=admin_auth_context.user.user_name,
+                )
+        with allure.step("Assert status code is 200"):
+            assert_status_code(response, 200)
+        with allure.step("Validate response schema and user_name"):
+            response_json = response.json()
+            parsed = UserResponseSchema.model_validate(response_json)
+            assert parsed.user_name == admin_auth_context.user.user_name
 
     @allure.title("Users: retrieve user by id")
-    async def test_get_user_by_id(self, api_client,admin_headers, admin_auth_context):
-        users_client = UserClient(api_client)
-
-        response = await users_client.get_user_by_id(
-            headers=admin_headers,
-            user_id=admin_auth_context.user.id,
-        )
+    @allure.severity(allure.severity_level.CRITICAL)
+    async def test_get_user_by_id(self, api_client,admin_headers, admin_auth_context, users_client):
+        with allure.step("Send POST /user?id=... request"):
+            response = await users_client.get_user_by_id(
+                headers=admin_headers,
+                user_id=admin_auth_context.user.id,
+                )
         assert_status_code(response, 200)
         response_json = response.json()
         parsed = UserResponseSchema.model_validate(response_json)
@@ -42,36 +45,30 @@ class TestUserService:
         assert parsed.user_name == admin_auth_context.user.user_name
 
     @allure.title("Users: retrieve all users")
-    async def test_get_all_user(self, api_client, admin_headers, admin_auth_context):
-        users_client = UserClient(api_client)
-
-        response = await users_client.get_all_users(
-            headers=admin_headers,
-
-        )
+    @allure.severity(allure.severity_level.NORMAL)
+    async def test_get_all_user(self, api_client, admin_headers, admin_auth_context, users_client):
+        with allure.step("Send POST /all_users... request"):
+             response = await users_client.get_all_users(
+                 headers=admin_headers,
+                 )
         assert_status_code(response, 200)
         response_json = response.json()
         parsed_users = [UserResponseSchema.model_validate(user) for user in response_json]
 
         assert len(parsed_users) >= 1
-        assert any(user.id == admin_auth_context.user.id for user in parsed_users)
+        #assert any(user.id == admin_auth_context.user.id for user in parsed_users)
+        assert all(isinstance(user.id, str) for user in parsed_users)
 
-    @allure.title("Users: get system roles")
-    async def test_get_system_roles(self, api_client, admin_headers, admin_auth_context):
-        users_client = UserClient(api_client)
-
-        response = await users_client.get_system_roles(
-            headers=admin_headers,
-
-        )
+    @pytest.mark.parametrize("role_name", ["admin", "user", "super_admin"])
+    @allure.title("Users: get system roles contains {role_name}")
+    @allure.severity(allure.severity_level.MINOR)
+    async def test_get_system_roles(self, api_client, admin_headers, admin_auth_context, users_client, role_name):
+        with allure.step("Send POST /system_roles... request"):
+            response = await users_client.get_system_roles(headers=admin_headers,)
         assert_status_code(response, 200)
-        #response_json = response.json()
         parsed = RolesResponseSchema.model_validate(response.json())
 
         role_names = [role.name for role in parsed.root]
-
-        assert "admin" in role_names
-        assert "user" in role_names
-        assert "super_admin" in role_names
+        assert role_name in role_names
 
 
